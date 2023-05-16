6723703f8f4fed6844ac87671aa29da611bfc344
Brenda Melo <brendarebeka.melo@gmail.com>
2023-05-16 17:06:02 -0300

    Estilizações iniciais

--- app-fast-checkout/src/app/app.module.ts
+++ app-fast-checkout/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
