d191319ffe361627f9bc5adcd97641fc08170d4e
Brenda Melo <brendarebeka.melo@gmail.com>
2023-05-18 15:59:28 -0300

    Novo componente pagamento

--- app-fast-checkout/src/app/Components/View/pagamento/pagamento.component.ts
+++ app-fast-checkout/src/app/Components/View/pagamento/pagamento.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagamento.component.scss']
 })
 export class PagamentoComponent {
-
+  dynamicRoutePay = '/pagamento';
+  btnPay = 'btnPay';
+  dynamicRouteBack = '/idClienteInstalacao';
+  btnBack = 'btnBack';
 }
