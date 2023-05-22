4cdad35ba75a67cab9208a7782153a13fa54b0ea
Brenda Melo <brendarebeka.melo@gmail.com>
2023-05-22 16:50:20 -0300

    feat: implementação de consumo dos dados Contract e Invoices

--- app-fast-checkout/src/app/Service/api.service.ts
+++ app-fast-checkout/src/app/Service/api.service.ts
@@ -7,15 +7,23 @@ import { Observable, tap } from 'rxjs';
 })
 export class ApiService {
 
-  private readonly baseURL = 'http://localhost:3001/'
+  // private readonly baseURL = 'http://localhost:3001/'
   private readonly urlContract = 'http://localhost:3001/contract-account'
 
   constructor(private http: HttpClient) { }
 
-  getContractAccount(numero: number): Observable<any> {
-    const params = new HttpParams().set("contract", numero);
-    return this.http.get<any>(this.urlContract, { params }).pipe(
-      tap((retorno) => console.log(retorno))
-    )
+  getContractAccount(document: string): Observable<any> {
+    const params = new HttpParams().set("contract", document);
+    return this.http.get<any>(this.urlContract, { params })
+  }
+  // getContractAccount(document: string): Observable<any> {
+  //   const params = new HttpParams().set("contract", document);
+  //   return this.http.get<any>(this.urlContract, { params }).pipe(
+  //     tap((retorno) => console.log(retorno))
+  //   )
+  // }
+
+  getInvoices() {
+    return this.http.get<any>('http://localhost:3001/invoices');
   }
 }
