6a8e8c1ba5ef3881440b346ab8991bfd5136dee0
Brenda Melo <brendarebeka.melo@gmail.com>
2023-05-23 11:22:46 -0300

    refactor: adpatação do input para pegar o formControlName

--- app-fast-checkout/src/app/Components/View/id-cliente-p1/id-cliente-p1.component.ts
+++ app-fast-checkout/src/app/Components/View/id-cliente-p1/id-cliente-p1.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChildren, QueryList } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from 'src/app/Service/api.service';
 import { InputComponent } from '../../input/input.component';
@@ -9,22 +9,19 @@ import { InputComponent } from '../../input/input.component';
   styleUrls: ['./id-cliente-p1.component.scss']
 })
 export class IdClienteP1Component {
-  @ViewChild('input', { static: false }) inputComponent!: InputComponent;
-
-  getinputValue() {
-    const inputValue = this.inputComponent.inputValue;
-    console.log('Valor do componente filho:', inputValue);
-  }
-
-  // formIdCliente: FormGroup = this.fb.group({
-  //   document: ['', Validators.required],
-  //   contract: ['', Validators.required]
-  // });
 
   constructor(private apiService: ApiService, private fb: FormBuilder) { }
+
+  form: FormGroup = this.fb.group({
+    document: ['', Validators.required],
+    contract: ['', Validators.required]
+  });
+
+  getInputValue() {
+    console.log(this.form.value);
+  }
 
   getContractAccount(data: any) {
     this.apiService.getContractAccount(data).subscribe((data) => console.log(data))
   }
-
 }
