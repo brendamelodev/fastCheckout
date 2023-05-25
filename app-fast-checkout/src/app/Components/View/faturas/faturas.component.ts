b7e0463235d23d8e7941e39963876308db5d2171
Brenda Melo <brendarebeka.melo@gmail.com>
2023-05-25 16:44:06 -0300

    feat: consumo api para projetão de faturas

--- app-fast-checkout/src/app/Components/View/faturas/faturas.component.ts
+++ app-fast-checkout/src/app/Components/View/faturas/faturas.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EMPTY, Subscription, catchError } from 'rxjs';
+import { ApiService } from 'src/app/Service/api.service';
+import { DataService } from 'src/app/Service/data.service';
 
 @Component({
   selector: 'app-faturas',
   templateUrl: './faturas.component.html',
   styleUrls: ['./faturas.component.scss']
 })
-export class FaturasComponent {
+export class FaturasComponent implements OnInit {
+  subscription?: Subscription;
+  invoiceId?: any;
+  invoiceById?: any;
+  accountContract?: any;
+  msgErro = '';
+
+  constructor(private dataService: DataService, private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.invoiceId = this.dataService.getInvoiceId();
+    this.accountContract = this.dataService.getAccountContract();
+    console.log(this.invoiceId);
+    this.getInvoiceById();
+  }
+
+  getInvoiceById() {
+    if (this.invoiceId != null && this.invoiceId != undefined) {
+      this.subscription = this.apiService.getInvoiceById(this.invoiceId).subscribe(
+        { next: data => {this.invoiceById = data; console.log(data);
+         }});
+    }
+    else {
+      console.log("Ops! Ocorreu um erro.");
+    }
+  }
+
 }
