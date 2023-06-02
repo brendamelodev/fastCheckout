8ef6d19e73d979db5437cb7b95022b29ffc19805
Brenda Melo <brendarebeka.melo@gmail.com>
2023-06-02 09:23:15 -0300

    feat: implementação de guardas para rotas

--- app-fast-checkout/src/app/Guards/auth.guard.ts
+++ app-fast-checkout/src/app/Guards/auth.guard.ts
@@ -1,5 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { DataService } from '../Service/data.service';
+import { LocalStorageService } from '../Service/local-storage.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const localStorageService: LocalStorageService = new LocalStorageService;
+  const sessionStorage = JSON.parse(localStorageService.getItem('contractAccount'));
+  const router: Router = new Router;
+
+  if (sessionStorage.length > 0) {
+    console.log('rota autorizada');
+    return true;
+  } else {
+    console.log('rota não autorizada');
+    router.navigate(['/idClienteP1']);
+    return false;
+  }
 };
