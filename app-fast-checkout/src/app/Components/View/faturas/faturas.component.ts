5993d400f344d60c68eb13c6d45afb836b0b3638
Brenda Melo <brendarebeka.melo@gmail.com>
2023-06-02 09:20:58 -0300

    refactor: ajuste de faturas.component para setar e imprimir os dados salvos da session storage

--- app-fast-checkout/src/app/Components/View/faturas/faturas.component.ts
+++ app-fast-checkout/src/app/Components/View/faturas/faturas.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { EMPTY, Subscription, catchError } from 'rxjs';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { ApiService } from 'src/app/Service/api.service';
-import { DataService } from 'src/app/Service/data.service';
 import { LocalStorageService } from 'src/app/Service/local-storage.service';
 
 @Component({
@@ -11,11 +11,7 @@ import { LocalStorageService } from 'src/app/Service/local-storage.service';
 })
 export class FaturasComponent implements OnInit {
   subscription?: Subscription;
-  invoiceId?: any;
-  invoiceById?: any;
-  accountContract?: any;
   selectedInvoices: any[] = [];
-  amount: any;
   msgErro = '';
 
   invoiceIdStorage: any;
@@ -24,41 +20,23 @@ export class FaturasComponent implements OnInit {
   totalAmountStorage: any;
   selectedInvoicesStorage: any[] = [];
 
-  constructor(private dataService: DataService, private apiService: ApiService, private localStorageService: LocalStorageService) { }
+  constructor(private localStorageService: LocalStorageService, private router: Router, private apiService: ApiService) { }
 
   ngOnInit() {
-    this.invoiceId = this.dataService.getInvoiceId();
     this.invoiceIdStorage = JSON.parse(this.localStorageService.getItem('invoiceId'));
-    this.accountContract = this.dataService.getAccountContract();
     this.accountContractStorage = JSON.parse(this.localStorageService.getItem('contractAccount'));
     this.invoiceByIdStorage = JSON.parse(this.localStorageService.getItem('invoiceById'));
-    console.log(this.invoiceByIdStorage);
-    this.getInvoiceById();
-  }
-
-  getInvoiceById() {
-    if (this.invoiceIdStorage != null && this.invoiceIdStorage != undefined) {
-      this.subscription = this.apiService.getInvoiceById(this.invoiceId).subscribe(
-        {
-          next: data => {
-            this.invoiceById = data; console.log(data);
-            this.dataService.setInvoiceById(data);
-            this.localStorageService.setItem('invoiceById', JSON.stringify(data));
-          }
-        });
-    }
-    else {
-      console.log("Ops! Ocorreu um erro.");
-    }
   }
 
   toggleInvoiceSelection(invoice: any) {
     const index = this.selectedInvoices.indexOf(invoice);
-    // const index = this.selectedInvoicesStorage.indexOf(invoice);
+    console.log(this.selectedInvoices);
+    console.log(this.selectedInvoices.indexOf(invoice));
+
 
     if (index !== -1) {
       this.selectedInvoices.splice(index, 1);
-      this.localStorageService.removeItem(index.toString());
+      this.localStorageService.removeItem(invoice);
     } else {
       this.selectedInvoices.push(invoice);
       this.localStorageService.setItem('selectedInvoiceIndex', JSON.stringify(invoice));
@@ -69,19 +47,11 @@ export class FaturasComponent implements OnInit {
 
   toggleAllInvoiceSelection() {
     if (this.selectedInvoicesStorage.length === this.invoiceByIdStorage.length) {
-      // this.selectedInvoices = [];
-      // this.invoiceById.forEach((invoice: any) => {
-      //   invoice.selected = false;
-      // });
       this.selectedInvoicesStorage = [];
       this.invoiceByIdStorage.forEach((invoice: any) => {
         invoice.selected = false;
       });
     } else {
-      // this.selectedInvoices = [...this.invoiceById];
-      // this.invoiceById.forEach((invoice: any) => {
-      //   invoice.selected = true;
-      // });
       this.selectedInvoicesStorage = [...this.invoiceByIdStorage];
       this.invoiceByIdStorage.forEach((invoice: any) => {
         invoice.selected = true;
@@ -94,11 +64,27 @@ export class FaturasComponent implements OnInit {
   }
 
   get totalAmount(): number {
-    const amount = this.selectedInvoices.reduce((total, invoice) => total + invoice.amount, 0);
-    this.dataService.setTotalAmount(amount);
-    this.amount = this.dataService.getTotalAmount();
+    const invoicesArray = Object.values(this.selectedInvoicesStorage);
+    const amount = invoicesArray.reduce((total, invoice) => total + invoice.amount, 0);
+
     this.localStorageService.setItem('totalAmount', JSON.stringify(amount));
     this.totalAmountStorage = JSON.parse(this.localStorageService.getItem('totalAmount'));
     return amount;
+  }
+
+  avancar() {
+    this.getInstallments();
+    setTimeout(() => {
+      this.router.navigateByUrl('/pagamento');
+    }, 50);
+  }
+
+  getInstallments() {
+    this.subscription = this.apiService.getInstallments().subscribe(
+      {
+        next: data => {
+          this.localStorageService.setItem('installments', JSON.stringify(data));
+        }
+      });
   }
 }
