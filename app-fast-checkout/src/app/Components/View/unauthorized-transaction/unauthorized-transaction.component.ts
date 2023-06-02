ebfe1a6df4df56a5a1421ea0beb832e17decab1a
Brenda Melo <brendarebeka.melo@gmail.com>
2023-06-02 15:00:06 -0300

    style: implementação do layout responsivo do componente unauthorized-transaction

--- app-fast-checkout/src/app/Components/View/unauthorized-transaction/unauthorized-transaction.component.ts
+++ app-fast-checkout/src/app/Components/View/unauthorized-transaction/unauthorized-transaction.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./unauthorized-transaction.component.scss']
 })
 export class UnauthorizedTransactionComponent {
-
+  cardIcon: string = 'https://cdn.icon-icons.com/icons2/2342/PNG/512/mastercard_payment_method_icon_142750.png';
+  textCard: string = 'Mastercard';
 }
